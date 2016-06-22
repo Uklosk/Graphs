ca224f862bb3e4e3fa27e222fceffc6c971e451b
LordVulkan <jomai92@gmail.com>
2016-06-22 13:02:40 +0200

    Updated gradle, SDK Tools and Android N compat.

--- Graphs/app/build/generated/source/buildConfig/debug/com/jmga/graphs/BuildConfig.java
+++ Graphs/app/build/generated/source/buildConfig/debug/com/jmga/graphs/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.jmga.graphs";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 7;
-  public static final String VERSION_NAME = "0.26_Beta";
+  public static final int VERSION_CODE = 8;
+  public static final String VERSION_NAME = "0.3";
 }
